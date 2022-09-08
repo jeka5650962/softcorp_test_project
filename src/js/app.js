ed276e177c91f6c2db60c88133ea80196929dcff
Evgeniy Salish <jeka5650962@gmail.com>
2022-09-08 17:29:58 +0300

    Продолжаю верстать форму UPD3

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,127 @@ import * as myFunctions from "./modules/functions.js";
 
 myFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Navigation, Pagination} from 'swiper';
 
 const swiper = new Swiper();
+
+// --------------------------------------------------------------------------- LINEBAR
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("rangeValue");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
+
+slider.addEventListener("mousemove", function () {
+    let x = slider.value;
+    slider.style.background = 'linear-gradient(90deg, rgb(62, 156, 220) ' + x + '%, rgb(189, 189, 192) ' + x + '%)';
+});
+
+// --------------------------------------------------------------------------- обработка формы
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+        formData.append('image', formImage.files[0]);
+
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+                form.classList.remove('_sending');
+            } else {
+                alert('Ошибка');
+                form.classList.remove('_sending');
+            }
+        } else {
+            alert('Заполните обязательные поля!');
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    // Функция теста email
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+
+    // Получаем инпут файл в переменную
+    const formImage = document.getElementById('formImage');
+    // Получаем div для превью в переменную
+    const formPreview = document.getElementById('uploadPreview');
+
+    // Слушаем изменения в input файл
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+    })
+
+    function uploadFile(file) {
+        // проверяем тип файла
+        if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+            alert('Разрешены только изображения!');
+            formImage.value = '';
+        }
+        // проверяем размер файла (до 2 МБ)
+        if (file.size > 2 * 1024 * 1024) {
+            alert('Файл должен быть менее 2 МБ');
+        }
+
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+        };
+        reader.onerror = function (e) {
+            alert('Ошибка');
+        };
+        reader.readAsDataURL(file);
+    }
+});
