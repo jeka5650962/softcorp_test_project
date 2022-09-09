a68421ff6a91973eab15e934094070d2e5078544
Evgeniy Salish <jeka5650962@gmail.com>
2022-09-09 12:34:25 +0300

    Верстаю select

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as myFunctions from "./modules/functions.js";
 import * as myRangeSlider from "./modules/rangeslider.js";
 import * as myForm from "./modules/form.js";
+import {Select} from "./modules/select/select.js";
 
 myFunctions.isWebp();
 myRangeSlider.rangeSlider();
@@ -9,3 +10,5 @@ myForm.form();
 import Swiper, {Navigation, Pagination} from 'swiper';
 
 const swiper = new Swiper();
+
+const select = new Select();
